15fb629d59a12ec6634e7b79e0c3aab9b40d28f5
yamiteru <yamiteru@icloud.com>
2023-03-08 01:02:46 +0100

    Include only dist folder in lib files

--- scripts/build.ts
+++ scripts/build.ts
@@ -2,14 +2,16 @@ import { build } from "esbuild";
 import { swcPlugin } from "esbuild-plugin-swc";
 
 build({
+  bundle: true,
   entryPoints: ["src/index.ts", "src/async.ts", "src/sync.ts"],
   plugins: [swcPlugin()],
-  bundle: true,
-  format: "esm",
+  logLevel: "info",
   minify: true,
   platform: "node",
   sourcemap: "linked",
   splitting: true,
   treeShaking: true,
   outdir: "dist",
+  format: "esm",
+  target: ["esnext", "node18.0.0"],
 });
